32fbf8cd812164d998b6f7aef201974326813142
Lidia Ordonez <lidiaordonez18@gmail.com>
2021-11-23 12:17:51 +1100

    Add access to Site DB + Modify Asset and AssetsComments (Models) to match respective tables from Site DB

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -12,13 +12,52 @@ class Asset extends Model implements Auditable
     use HasFactory;
 
     /**
+     * The primary key associated with the table.
+     * 
+     * This section probably will not be needed with new table
+     *
+     * @var string
+     */
+    protected $primaryKey = 'internal_id';
+
+    /**
+     * The connection name for the model.
+     * 
+     * This section will not be needed with new table
+     *
+     * @var string
+     */
+    protected $connection = 'site';
+
+    /**
+     * The table associated with the model.
+     *
+     * This section will not be needed with new table
+     * 
+     * @var string
+     */
+    protected $table = 'asset_register';
+
+    /**
+     * No timestamps for this table
+     *      
+     * This section probably will not be needed with new table
+     * 
+     */
+    public $timestamps = false;
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'tag', 'location', 'type', 'make', 'model', 'identifier', 'supplier', 'supply_date', 'serial_no', 'other_ids', 'invoice_no'
+        'asset_id', 'location', 'type', 'make', 'model', 'identifier', 'supplier', 'supply_date', 'serial_no', 'other_ids', 'invoice_no'
     ];
+    //New Version
+    // protected $fillable = [
+    //     'tag', 'location', 'type', 'make', 'model', 'identifier', 'supplier', 'supply_date', 'serial_no', 'other_ids', 'invoice_no'
+    // ];
 
     /**
      * Get the comments for the asset
--- app/Models/AssetComments.php
+++ app/Models/AssetComments.php
@@ -12,13 +12,59 @@ class AssetComments extends Model implements Auditable
     use HasFactory;
 
     /**
+     * The primary key associated with the table.
+     * 
+     * This section probably will not be needed with new table
+     *
+     * @var string
+     */
+    protected $primaryKey = 'internal_id';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The connection name for the model.
+     * 
+     * This section will not be needed with new table
+     *
+     * @var string
+     */
+    protected $connection = 'site';
+
+    /**
+     * The table associated with the model.
+     *
+     * This section will not be needed with new table
+     * 
+     * @var string
+     */
+    protected $table = 'asset_comments';
+
+     /**
+     * No timestamps for this table
+     *      
+     * This section probably will not be needed with new table
+     * 
+     */
+    public $timestamps = false;
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'asset_id', 'dt', 'cat', 'comments'
+        'internal_id','dt', 'cat', 'comments'
     ];
+    // New Version
+    // protected $fillable = [
+    //     'asset_id', 'dt', 'cat', 'comments'
+    // ];
 
     /**
      * Get the asset that owns the comment.
